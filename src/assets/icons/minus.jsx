0c670ccfca92dffaef38f537d2647490dc9ec180
Vladimir Le1to <le1to@LK-MBP.local>
2023-12-14 17:31:11 +0300

    assets: added + & - icons in svg jsx

---
+++ src/assets/icons/minus.jsx
@@ -0,0 +1,26 @@
+const Minus = ({width, height}) => {
+  return (
+    <svg
+      version="1.0"
+      width={width}
+      height={height}
+      viewBox="0 0 512.000000 512.000000"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <g
+        transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+        fill="#d0d5f5"
+        stroke="none"
+      >
+        <path
+          d="M233 3000 c-93 -33 -156 -92 -200 -185 -28 -59 -28 -62 -28 -260 l0
+-200 33 -67 c38 -77 92 -130 171 -167 l56 -26 2295 0 2295 0 56 26 c79 37 133
+90 171 167 l33 67 0 200 c0 199 0 200 -29 262 -33 73 -103 143 -173 176 l-48
+22 -2290 2 c-2222 2 -2292 2 -2342 -17z"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default Minus;
