a8e8a08c3c7098db430c8c33f9035bdba28e40d2
Valdimir_MSI <None>
2023-12-15 16:35:26 +0300

    feat: changed fex jsx, added task toggling

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,11 +1,11 @@
 import TaskItem from '../TaskItem';
 import styles from './TaskList.module.scss'
 
-const TaskList = ({ tasks, deleteTask }) => {
+const TaskList = ({ tasks, deleteTask, toggleTask }) => {
   return (
     <ul className={styles.tasks}>
       {tasks.map((item) => (
-        <TaskItem key={item.id} task={item} deleteTask={deleteTask} />
+        <TaskItem key={item.id} task={item} deleteTask={deleteTask} toggleTask={toggleTask} />
       ))}
     </ul>
   );
