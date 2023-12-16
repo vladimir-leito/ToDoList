6b9b24e77f1416df583e0e7155c51266a1e8cb93
Valdimir_MSI <None>
2023-12-16 16:40:42 +0300

    fix: fixed tabs and added few styles

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,11 +1,16 @@
 import TaskItem from '../TaskItem';
-import styles from './TaskList.module.scss'
+import styles from './TaskList.module.scss';
 
 const TaskList = ({ tasks, deleteTask, toggleTask }) => {
   return (
     <ul className={styles.tasks}>
       {tasks.map((item) => (
-        <TaskItem key={item.id} task={item} deleteTask={deleteTask} toggleTask={toggleTask} />
+        <TaskItem
+          key={item.id}
+          task={item}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+        />
       ))}
     </ul>
   );
