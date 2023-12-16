ca553bc005fdb343c438ce428000480f10f4ef65
Valdimir_MSI <None>
2023-12-16 16:42:04 +0300

    feat: FilterBar integration

--- src/components/InputForm/index.jsx
+++ src/components/InputForm/index.jsx
@@ -1,27 +1,28 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import Plus from "../../assets/icons/plus.jsx";
-import "./InputForm.scss";
+import Plus from '../../assets/icons/plus.jsx';
+import styles from './InputForm.module.scss';
 
 const InputForm = ({ addTask }) => {
-  const [task, setTask] = useState("");
+  const [task, setTask] = useState('');
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     addTask({
       name: task,
       checked: false,
+      filter: 'all',
       id: Date.now(),
     });
-    setTask("");
+    setTask('');
   };
 
   return (
     <>
-      <form className="todo" onSubmit={handleFormSubmit}>
-        <div className="wrapper">
+      <form className={styles.todo} onSubmit={handleFormSubmit}>
+        <div className={styles.wrapper}>
           <input
-            className="input"
+            className={styles.input}
             type="text"
             placeholder=""
             value={task}
@@ -32,11 +33,11 @@ const InputForm = ({ addTask }) => {
               setTask(e.target.value);
             }}
           />
-          <div className="cut"></div>
-          <label htmlFor="Enter task" className="placeholder">
+          <div className={styles.cut}></div>
+          <label htmlFor="Enter task" className={styles.placeholder}>
             Enter task
           </label>
-          <button className="submit" type="submit">
+          <button className={styles.submit} type="submit">
             <Plus width="1.2rem" height="1.2rem" />
           </button>
         </div>
